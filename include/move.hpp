8e0f23ea3d0cf4b9179b901128208e6a459bd521
imvoid4 <ifbroken.yt@gmail.com>
2026-07-26 18:14:05 +0100

    Added win checker

--- include/move.hpp
+++ include/move.hpp
@@ -5,9 +5,9 @@ inline int board[42];
 // 1: Red
 // 2: Yellow
 
-constexpr int BLANK = 0;
-constexpr int RED = 1;
-constexpr int YELLOW = 2;
+constexpr int _BLANK = 0;
+constexpr int _RED = 1;
+constexpr int _YELLOW = 2;
 
 inline bool MakeMove(int col, int player) {
     if (col < 0 || col >= 7) return false;
@@ -24,5 +24,48 @@ inline bool MakeMove(int col, int player) {
 }
 
 inline void UnMove(int row, int col) {
-    board[row*7+col] = BLANK;
+    board[row*7+col] = _BLANK;
+}
+
+inline bool InBounds(int row, int col) {
+    return row >= 0 && row < 6 && col >= 0 && col < 7;
+}
+
+inline int CountDirection(int row, int col, int dr, int dc, int player) {
+    int count = 0;
+
+    row += dr;
+    col += dc;
+
+    while (InBounds(row, col) && board[row * 7 + col] == player) {
+        count++;
+        row += dr;
+        col += dc;
+    }
+    return count;
+}
+
+inline bool CheckWin(int row, int col) {
+    int player = board[row * 7 + col];
+
+    if (player == _BLANK) return false;
+
+    constexpr int dirs[4][2] = {
+        {0, 1},   // -
+        {1, 0},   // |
+        {1, 1},   // \      . <- no touchy
+        {1,-1}    // /
+    };
+
+    for (auto& d : dirs) {
+        int count = 1;
+
+        count += CountDirection(row, col, d[0], d[1], player);
+        count += CountDirection(row, col, -d[0], -d[1], player);
+
+        if (count >= 4)
+            return true;
+    }
+
+    return false;
 }
